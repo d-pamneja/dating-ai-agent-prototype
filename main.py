368cd901b5199a32451e8124a7eec7d34ddd3a54
d-pamneja <dpamneja@gmail.com>
2024-12-14 19:55:02 +0530

    Deployment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Importing the dependencies
-import uvicorn
+from mangum import Mangum
 from api.resource import app
 
 # Running the App with Uvicorn
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    handler = Mangum(app)
