26879703e754ab640d31fd25a2fa281d001a1f71
d-pamneja <dpamneja@gmail.com>
2024-12-14 22:23:48 +0530

    Deployment

--- api/resource.py
+++ api/resource.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://dating-ai-agent-prototype-frontend-ffvcxdyvt.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
